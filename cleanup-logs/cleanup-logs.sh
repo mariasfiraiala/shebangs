e466c55035a49c73988694bf0cc4bf606db65ebb
Maria Sfiraiala <maria.sfiraiala@gmail.com>
2022-09-25 22:07:51 +0300

    Add comments to cleanup logs script
    Signed-off-by: Maria Sfiraiala <maria.sfiraiala@gmail.com>

--- cleanup-logs/cleanup-logs.sh
+++ cleanup-logs/cleanup-logs.sh
@@ -2,7 +2,6 @@
 
 LOG_DIR=/var/log
 ROOT_UID=0
-LINES=50
 
 E_BADARGS=65
 E_CANT_CD=66
@@ -15,6 +14,9 @@ if [ "$UID" -ne "$ROOT_UID" ]; then
     exit $E_NOTROOT
 fi
 
+# The script either has a number as an argument or nothing
+# use case in order to check that
+
 case $1 in
     "" )        lines=50 ;;
     *[!0-9]* )  echo "Usage: $PROGNAME lines";
@@ -24,10 +26,15 @@ esac
 
 cd $LOG_DIR
 
+# if, for some reason, we were unable to change directories, prompt the user
+# and exit gracefully
+
 if [ "$PWD" != "$LOG_DIR" ]; then
     echo "Can't change directory to $LOG_DIR"
     exit $E_CANT_CD
 fi
+
+# Back to business: clear the logs
 
 tail -$lines messages > msg.temp
 mv msg.temp messages
