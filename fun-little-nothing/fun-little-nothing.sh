7481a393d85e5b2268d35ee7e7c0e53c0a1777eb
Maria Sfiraiala <maria.sfiraiala@gmail.com>
2022-10-30 09:11:34 +0200

    Add comments to fun little nothing script
    Signed-off-by: Maria Sfiraiala <maria.sfiraiala@gmail.com>

--- fun-little-nothing/fun-little-nothing.sh
+++ fun-little-nothing/fun-little-nothing.sh
@@ -1,5 +1,9 @@
 #!/bin/rm
 
+# This educational script proves how shebangs really work.
+# By giving the script the path to rm, we are, basically, invoking it,
+# therefore the file will disappear long before printing the message
+
 RANDOM_EXIT=65
 
 echo "Will I ever see the light of the terminal?!"
