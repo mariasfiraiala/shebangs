583c233bf6a820f76116ba9c5f3aca973952b35b
Maria Sfiraiala <maria.sfiraiala@gmail.com>
2022-11-04 21:26:36 +0200

    Add comments to kill process by name script
    Signed-off-by: Maria Sfiraiala <maria.sfiraiala@gmail.com>

--- kill-by-name/kill-by-name.sh
+++ kill-by-name/kill-by-name.sh
@@ -4,12 +4,18 @@ E_WRONGARGS=66
 
 PROGNAME=$(basename $0)
 
+# The script must have one argument, the name of the process we want killed
+
 if [ -z "$1" ]; then
     echo "Usage: $PROGNAME process"
     exit $E_WRONGARGS
 fi
 
 PROCESS_NAME="$1"
+
+# ps ax -> see every process on the system using BSD syntax
+# xargs -i kill {} -> kills every process found by awk
+
 ps ax | grep "$PROCESS_NAME" | awk '{ print $1 }' | xargs -i kill {} 2&>/dev/null
 
 exit $?
