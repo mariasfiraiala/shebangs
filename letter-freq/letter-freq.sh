e60b94e3a0542bd360b7173c9f094f42a2245f3b
Maria Sfiraiala <maria.sfiraiala@gmail.com>
2022-11-04 21:37:38 +0200

    Add comments to get frequency of letters for a file script
    Signed-off-by: Maria Sfiraiala <maria.sfiraiala@gmail.com>

--- letter-freq/letter-freq.sh
+++ letter-freq/letter-freq.sh
@@ -9,6 +9,9 @@ fi
 
 FILENAME=$1
 
+# read characher by character from the given file
+# grep "[[:alpha:]]" -> gets the alphabetic characters
+# uniq -c -> counts the occurances of the letters
 while read -n 1 ch; do
     echo "$ch"
 done < "$FILENAME" | grep "[[:alpha:]]" | sort | uniq -c | sort -nr
